65756663e0c6cc4845c462a638d663224cd17ba6
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2022-12-12 04:00:36 -0500

    commit tree 844bbd7252ffe00e62ae6aa0110de12e30009c08

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+    let messages = [];
+    for(let i = 0; i < names.length; i++){
+        let msg = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(msg);       
+    }
+    return messages;
+}
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n--);
+    }
+
+    
+}
+
